bb28caed886db36848ada8383c870d545d1683d7
HoHyung2 <pokow@naver.com>
2020-06-10 16:20:57 +0900

    init commit

---
+++ src/connect/Practice.java
@@ -0,0 +1,17 @@
+package connectgit;
+
+import java.util.Scanner;
+
+public class Practice {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("a입력");
+		int a = sc.nextInt();
+		System.out.println("b입력");
+		int b = sc.nextInt();
+		
+		System.out.println(a);
+		System.out.println(b);
+	}
+}
